1fd75e534543ae0e8f2a160baf233cd093809610
YareArista <yarearista@gmail.com>
2016-11-18 15:37:12 -0600

    portafolio no terminada, faltan proyectos y contacto

---
+++ js/cerrarpreloading.js
@@ -0,0 +1,9 @@
+var container = document.getElementById('conte');
+var body = document.getElementById('bod');
+
+setTimeout(function(){
+    container.classList.add('cerrar');
+    body.classList.add('bod');
+
+
+},4500);
